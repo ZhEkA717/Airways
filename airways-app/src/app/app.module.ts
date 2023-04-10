b3049f6f2ff3a87a416ffb101e0708f5ffa48480
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-04-10 13:25:00 +0300

    feat: add auth components and service

--- airways-app/src/app/app.module.ts
+++ airways-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import CoreModule from './core/core.module';
+import LoginComponent from './auth/login/login.component';
+import RegistrationComponent from './auth/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
