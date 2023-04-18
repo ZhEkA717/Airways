485d0d4eef4588ad88e8c4dcb275e0858a94d5b9
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-04-18 13:29:24 +0300

    fix: changes naming

--- airways-app/src/app/redux/actions/search.action.ts
+++ airways-app/src/app/redux/actions/search.action.ts
@@ -2,10 +2,10 @@ import { createAction } from '@ngrx/store';
 import { FlightSearch } from 'src/app/main/model/flight-search.model';
 
 export enum SearchAction {
-  send = '[SEARCH] send form',
+  sendForm = '[SEARCH] send form',
 }
 
 export const send = createAction(
-  SearchAction.send,
+  SearchAction.sendForm,
   (searchForm: FlightSearch) => searchForm,
 );
