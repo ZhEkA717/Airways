f4c8e7c2144aa60895e4c315febe887ea476e324
EXisTAnZ <ozim_ssspam@mail.ru>
2023-04-26 17:19:34 +0300

    feat: provide interceptor in core module

--- airways-app/src/app/core/core.module.ts
+++ airways-app/src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import HeaderComponent from './components/header/header.component';
 import FooterComponent from './components/footer/footer.component';
 import SharedModule from '../shared/shared.module';
 import FormatService from './services/format.service';
+import HttpApiInterceptor from './services/http-api.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,13 @@ import FormatService from './services/format.service';
     HeaderComponent,
     FooterComponent,
   ],
-  providers: [FormatService],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpApiInterceptor,
+      multi: true,
+    },
+    FormatService,
+  ],
 })
 export default class CoreModule { }
