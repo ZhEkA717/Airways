78397be3cad3b4ab1ae7ec0572a0999b49418e26
EXisTAnZ <ozim_ssspam@mail.ru>
2023-04-27 15:23:32 +0300

    feat: parsing information from JWT tokens

--- airways-app/src/app/auth/services/auth.service.ts
+++ airways-app/src/app/auth/services/auth.service.ts
@@ -4,5 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export default class AuthService {
+  private authToken: string;
 
+  constructor() {
+    this.authToken = '';
+    console.log(this.parseJwt('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImN1c3RvbWVyMUBhaXJ3YXlzLmNvbSIsImlhdCI6MTY4MjU5NzkzOSwiZXhwIjoxNjgyNjAxNTM5LCJzdWIiOiIyIn0.4GlrdumIVG9UujqefGXYBHo3V7QJtYYFZCgccQtep9Y'));
+  }
+
+  private parseJwt(token:string) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    try {
+      const jsonPayload = decodeURIComponent(window.atob(base64).split('').map((c) => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`).join(''));
+      return JSON.parse(jsonPayload);
+    } catch (error) {
+      return 'Token validation error';
+    }
+  }
 }
