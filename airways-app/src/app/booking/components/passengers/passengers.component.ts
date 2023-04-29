b1bd6f9ec3bb96dcae8106c20e53af4f6e3df5cd
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-04-29 18:56:33 +0300

    feat: add dynamic passengers forms

--- airways-app/src/app/booking/components/passengers/passengers.component.ts
+++ airways-app/src/app/booking/components/passengers/passengers.component.ts
@@ -1,17 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import HeaderService from 'src/app/core/services/header.service';
+import { FlightSearch } from 'src/app/main/model/flight-search.model';
+import { selectSearch } from 'src/app/redux/selectors/search.selector';
+import { Passenger } from 'src/app/shared/model/persons.model';
 
 @Component({
   selector: 'app-passengers',
   templateUrl: './passengers.component.html',
   styleUrls: ['./passengers.component.scss'],
 })
-export default class PassengersComponent implements OnInit {
-  constructor(private headerService: HeaderService) {}
+export default class PassengersComponent implements OnInit, OnDestroy {
+  private searchForm$ = this.store.select(selectSearch);
+
+  private subSearchForm!: Subscription;
+
+  public passengersName: string[] = [];
+
+  public form = new FormGroup({
+    passengers: new FormArray([]),
+  });
+
+  constructor(private headerService: HeaderService, private store: Store) {}
 
   ngOnInit(): void {
     this.headerService.setStepper({
-      flight: true, passengers: true, review: false,
+      flight: true,
+      passengers: true,
+      review: false,
     });
+
+    this.subSearchForm = this.searchForm$.subscribe((res) => {
+      this.fillPassengersName(res);
+    });
+
+    this.fillPassengersFormGroup();
+  }
+
+  ngOnDestroy(): void {
+    this.subSearchForm.unsubscribe();
+  }
+
+  get passengers() {
+    return this.form.get('passengers') as FormArray;
+  }
+
+  private fillPassengersName(search: FlightSearch) {
+    search.passengers.forEach((item) => {
+      for (let i = 0; i < item.value; i += 1) {
+        this.passengersName.push(item.view);
+      }
+    });
+  }
+
+  private fillPassengersFormGroup() {
+    this.passengersName.forEach(() => {
+      this.passengers.push(this.createPassenger());
+    });
+  }
+
+  createPassenger() {
+    return new FormGroup({
+      firstName: new FormControl('', [
+        Validators.required,
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+      ]),
+      gender: new FormControl('', [
+        Validators.required,
+      ]),
+      date: new FormControl('', [
+        Validators.required,
+      ]),
+      isCripple: new FormControl('', [
+        Validators.required,
+      ]),
+    });
+  }
+
+  submit() {
+    const form = this.passengers.value
+      .map((item: Passenger, i: number) => ({
+        ...item,
+        type: this.passengersName[i],
+      }));
+
+    // eslint-disable-next-line no-console
+    console.log(form);
   }
 }
