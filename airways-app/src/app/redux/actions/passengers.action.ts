0bdd5472cfb61f728b54ce7dc847945205a2425c
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-05-02 08:58:16 +0300

    fix: deleted actions in passengers

--- airways-app/src/app/redux/actions/passengers.action.ts
+++ airways-app/src/app/redux/actions/passengers.action.ts
@@ -3,8 +3,6 @@ import { PassengersForm } from 'src/app/booking/models/passengers.model';
 
 export enum PassengersAction {
   sendForm = '[PASSENGERS] send form',
-  getAirportSuccess = '[Airports] get airports success',
-  getAirportError = '[Airports] get airports error',
 }
 
 export const send = createAction(
