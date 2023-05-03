fab346bfdfd00d70748aac7261846473d2201838
EXisTAnZ <ozim_ssspam@mail.ru>
2023-05-03 21:14:38 +0300

    feat: generate cart module and page

---
+++ airways-app/src/app/cart/cart.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CartRoutingModule } from './cart-routing.module';
+import { CartPageComponent } from './cart-page/cart-page.component';
+
+@NgModule({
+  declarations: [CartPageComponent],
+  imports: [
+    CommonModule,
+    CartRoutingModule,
+  ],
+})
+export default class CartModule { }
