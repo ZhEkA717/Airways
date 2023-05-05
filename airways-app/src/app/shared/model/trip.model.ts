1cbe9e458c77bbd38adbbfa179e3906d968fa379
EXisTAnZ <ozim_ssspam@mail.ru>
2023-05-05 11:38:03 +0300

    feat: add city names to trip

--- airways-app/src/app/shared/model/trip.model.ts
+++ airways-app/src/app/shared/model/trip.model.ts
@@ -1,8 +1,8 @@
 export interface Trip {
   id: string;
   flightNo: string;
-  from: string;
-  to: string;
+  from: { code: string; city: string; };
+  to: { code: string; city: string; };
   departDate: string;
   flightTime: string;
   arrivDate: string;
