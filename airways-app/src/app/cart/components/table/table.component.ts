988d959091b922871f123175589a437631a22e52
EXisTAnZ <ozim_ssspam@mail.ru>
2023-05-06 23:09:22 +0300

    feat: add footer for table

--- airways-app/src/app/cart/components/table/table.component.ts
+++ airways-app/src/app/cart/components/table/table.component.ts
@@ -21,8 +21,8 @@ export interface Flight {
 export interface MockFlight {
   id: number,
   flightNo: string;
-  to: { city: string; dateTime: string };
-  from: { city: string; dateTime: string };
+  forward: { flight: string; datetime: string };
+  backward: { flight: string; datetime: string };
   type: FlightType;
   passengers: string[];
   price: string
@@ -30,13 +30,13 @@ export interface MockFlight {
 
 const ELEMENT_DATA: MockFlight[] = [
   {
-    id: 1, flightNo: 'BS1416', to: { city: 'Brussels', dateTime: '13.05.2023 14:50:00 - 21:00:00' }, from: { city: 'Berlin', dateTime: '14.05.2023 8:00:00 - 14:10:00' }, type: 'Round trip', passengers: ['1 x Adult', '1 x Child', '1 x Infant'], price: '$551.38',
+    id: 1, flightNo: 'BS1416', forward: { flight: 'Berlin - Bruxselles', datetime: '13.05.2023 14:50:00 - 21:00:00' }, backward: { flight: 'Bruxselles - Berlin', datetime: '14.05.2023 8:00:00 - 14:10:00' }, type: 'Round trip', passengers: ['1 x Adult', '1 x Child', '1 x Infant'], price: '$551.38',
   },
   {
-    id: 2, flightNo: 'BT948', to: { city: 'Barselona', dateTime: '16.05.2023 8:00:00 - 12:10:00' }, from: { city: 'Berlin', dateTime: '16.05.2023 18:00:00 - 22:10:00' }, type: 'One way', passengers: ['1 x Adult'], price: '$20.96',
+    id: 2, flightNo: 'BT948', forward: { flight: 'Berlin - Barselona', datetime: '16.05.2023 8:00:00 - 12:10:00' }, backward: { flight: '', datetime: '' }, type: 'One way', passengers: ['1 x Adult', '', ''], price: '$20.96',
   },
   {
-    id: 3, flightNo: 'CP1661', to: { city: 'Tel Aviv', dateTime: '18.05.2023 17:40:00 - 22:40:00' }, from: { city: 'Barselona', dateTime: '19.05.2023 8:00:00 - 13:00:00' }, type: 'One way', passengers: ['1 x Adult', '1 x Child'], price: '$192.15',
+    id: 3, flightNo: 'CP1661', forward: { flight: 'Barselona - Tel Aviv', datetime: '18.05.2023 17:40:00 - 22:40:00' }, backward: { flight: '', datetime: '' }, type: 'One way', passengers: ['1 x Adult', '1 x Child', ''], price: '$192.15',
   },
 ];
 
