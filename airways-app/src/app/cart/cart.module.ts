463bb78f26d01e5b9d87ba4506c05ac7291ebeea
EXisTAnZ <ozim_ssspam@mail.ru>
2023-05-07 16:36:15 +0300

    feat: add promo component

--- airways-app/src/app/cart/cart.module.ts
+++ airways-app/src/app/cart/cart.module.ts
@@ -4,12 +4,16 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { CartRoutingModule } from './cart-routing.module';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { TableComponent } from './components/table/table.component';
+import { PromoComponent } from './components/promo/promo.component';
 
 @NgModule({
-  declarations: [CartPageComponent, TableComponent],
+  declarations: [CartPageComponent, TableComponent, PromoComponent],
   imports: [
     CommonModule,
     CartRoutingModule,
@@ -17,6 +21,9 @@ import { TableComponent } from './components/table/table.component';
     MatCheckboxModule,
     MatMenuModule,
     MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
   ],
   exports: [
   ],
