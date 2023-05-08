421e29f23b2b02dfbe26c5014a8bc8dfd4a8cdf0
EXisTAnZ <ozim_ssspam@mail.ru>
2023-05-08 21:51:43 +0300

    refactor: move material imports to shared module

--- airways-app/src/app/cart/cart.module.ts
+++ airways-app/src/app/cart/cart.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
+import SharedModule from '../shared/shared.module';
 import { CartRoutingModule } from './cart-routing.module';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { TableComponent } from './components/table/table.component';
@@ -17,13 +11,7 @@ import { PromoComponent } from './components/promo/promo.component';
   imports: [
     CommonModule,
     CartRoutingModule,
-    MatTableModule,
-    MatCheckboxModule,
-    MatMenuModule,
-    MatIconModule,
-    MatInputModule,
-    MatButtonModule,
-    FormsModule,
+    SharedModule,
   ],
   exports: [
   ],
