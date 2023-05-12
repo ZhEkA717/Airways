50642384ccfba71be370abc12381297c776d8410
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-05-12 23:39:45 +0300

    feat: add mock data for disabled seats

--- airways-app/src/app/booking/components/reserved-seats/reserved-seats.component.ts
+++ airways-app/src/app/booking/components/reserved-seats/reserved-seats.component.ts
@@ -19,6 +19,10 @@ export default class ReservedSeatsComponent implements OnInit {
 
   public arraySeats: string[][] = [];
 
+  public reservedThere = ['1A', '1B', '1C', '2E', '2F', '12C', '14D', '18F']; // mock data
+
+  public reservedBack = ['1A', '15E', '15F', '14C', '14D', '20F', '20E']; // mock data
+
   private get getArraySeats() {
     const arr: string[][] = [];
     this.seatsNumbers.forEach((item) => {
