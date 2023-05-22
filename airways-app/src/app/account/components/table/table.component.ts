3b0f8edf5b32a34a797e3d7ca9732181cf385c9b
EXisTAnZ <ozim_ssspam@mail.ru>
2023-05-22 13:13:30 +0300

    feat: get user info from backend

--- airways-app/src/app/account/components/table/table.component.ts
+++ airways-app/src/app/account/components/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
 import { CartItem } from '../../../shared/model/cart.model';
 import ConvertMoneyService from '../../../booking/services/convert-money.service';
 import { selectBuyedItems } from '../../../redux/selectors/cart.selector';
@@ -15,7 +16,11 @@ export class TableComponent {
 
   dataSource: MatTableDataSource<CartItem>;
 
-  constructor(public currencyService: ConvertMoneyService, private store: Store) {
+  constructor(
+    public currencyService: ConvertMoneyService,
+    private store: Store,
+    private router: Router,
+  ) {
     this.dataSource = new MatTableDataSource<CartItem>([]);
     store.select(selectBuyedItems).subscribe((res) => { this.dataSource.data = res; });
   }
@@ -23,5 +28,6 @@ export class TableComponent {
   /** Onclick row from table */
   goSummary(row: CartItem) {
     console.log('Need to implement goSummary', row);
+    this.router.navigate(['/booking', 'review', { fromAccount: true }]);
   }
 }
