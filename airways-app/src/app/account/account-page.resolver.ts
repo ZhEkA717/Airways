a1bbdb13456395853efc2d2bd0d49c7de519ba88
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-05-23 09:08:16 +0300

    feat: add profile resolver

---
+++ airways-app/src/app/account/account-page.resolver.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Router, Resolve } from '@angular/router';
+import {
+  EMPTY,
+  Observable,
+  catchError,
+} from 'rxjs';
+import { User } from '../shared/model/persons.model';
+import AuthService from '../auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export default class AccountPageResolver implements Resolve<User> {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  resolve(): Observable<User> {
+    return this.authService.getCurrentUser()
+      .pipe(
+        catchError(() => {
+          this.router.navigate(['main']);
+          return EMPTY;
+        }),
+      );
+  }
+}
--- airways-app/src/app/account/account-routing.module.ts
+++ airways-app/src/app/account/account-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountPageComponent } from './account-page/account-page.component';
+import AccountPageResolver from './account-page.resolver';
 
 const routes: Routes = [
-  { path: '', component: AccountPageComponent },
+  {
+    path: '',
+    component: AccountPageComponent,
+    resolve: {
+      currentUser: AccountPageResolver,
+    },
+  },
 ];
 
 @NgModule({
